f3d213f5efa308fe447b4854e9ca0fb26b373f38
Murilo <silveiragmurilo@gmail.com>
2026-04-16 21:38:45 -0300

    logica do contador

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
 <template>
   <div>
     <h2>Contador de Cliques</h2>
-    <p>Você clicou <strong>0</strong> vezes</p>
-    <button>Clique aqui</button>
+    <p>Você clicou <strong>{{ count }}</strong> vezes</p>
+    <button @click="increment">Clique aqui</button>
   </div>
 </template>
